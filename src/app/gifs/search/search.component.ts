4e5cfc6b0b037c5185a35eba6c01f40c3d40c396
jonathan garzon <jonatangarzon95@gmail.com>
2021-12-04 07:27:26 -0500

    initial commit

---
+++ src/app/gifs/search/search.component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styles: [
+  ]
+})
+export class SearchComponent {
+
+  @ViewChild("txtSearch") txtSearch!: ElementRef<HTMLInputElement>;
+  search() {
+    const value = this.txtSearch.nativeElement.value;
+    console.log(value);
+    this.txtSearch.nativeElement.value = "";
+  }
+
+}
